60d4c639d3083453e7ee04c4e9f160577e28b5d8
Teddy K <teddy@communic8agency.com>
2020-02-02 19:40:15 +0700

    create api get user by id

--- routes/users.js
+++ routes/users.js
@@ -9,7 +9,32 @@ router.get('/', async function(req, res, next) {
     if (users.length !== 0) {
       res.json({
         'status': 'OK',
-        'messages': '',
+        'data': users
+      })
+    } else {
+      res.json({
+        'status': 'ERROR',
+        'messages': 'EMPTY',
+        'data': {}
+      })
+    }
+  } catch (err) {
+    res.json({
+      'status': 'ERROR',
+      'messages': err.messages,
+      'data': {}
+    })
+  }
+});
+
+/* GET users listing. */
+router.get('/:id', async function(req, res, next) {
+  try {
+    const usersId = req.params.id;
+    const users = await model.users.findOne({ where: { id: usersId } });
+    if (users.length !== 0) {
+      res.json({
+        'status': 'OK',
         'data': users
       })
     } else {
@@ -29,15 +54,92 @@ router.get('/', async function(req, res, next) {
 });
 
 // POST users
-router.post('/', function(req, res, next) {
+router.post('/', async function (req, res, next) {
+  try {
+    const {
+      name,
+      email,
+      gender,
+      phoneNumber
+    } = req.body;
+    const users = await model.users.create({
+      name,
+      email,
+      gender,
+      phone_number: phoneNumber
+    });
+  if (users) {
+    res.status(201).json({
+      'status': 'OK',
+      'messages': 'User berhasil ditambahkan',
+      'data': users,
+    })
+  }
+ } catch (err) {
+   res.status(400).json({
+     'status': 'ERROR',
+     'messages': err.message,
+     'data': {},
+   })
+ }
 });
 
-// UPDATE users
-router.patch('/:id', function(req, res, next) {
+// Edit users
+router.patch('/:id', async function (req, res, next) {
+  try {
+    const usersId = req.params.id;
+    const {
+      name,
+      email,
+      gender,
+      phoneNumber
+    } = req.body;
+    const users = await model.users.update({
+      name,
+      email,
+      gender,
+      phone_number: phoneNumber
+    }, {
+      where: {
+        id: usersId
+      }
+    });
+    if (users) {
+      res.json({
+        'status': 'OK',
+        'messages': 'User berhasil diupdate',
+        'data': users,
+      })
+    }
+  } catch (err) {
+    res.status(400).json({
+      'status': 'ERROR',
+      'messages': err.message,
+      'data': {},
+    })
+  }
 });
 
-// DELETE users
-router.delete('/:id', function(req, res, next) {
+// Delete users
+router.delete('/:id', async function (req, res, next) {
+  try {
+    const usersId = req.params.id;
+    const users = await model.users.destroy({ where: {
+      id: usersId
+    }})
+    if (users) {
+      res.json({
+        'status': 'OK',
+        'messages': 'User berhasil dihapus',
+        'data': users,
+      })
+    }
+  } catch (err) {
+    res.status(400).json({
+      'status': 'ERROR',
+      'messages': err.message,
+      'data': {},
+    })
+  }
 });
-
 module.exports = router;
